d1700f3f9a7b8f92d38ea60a2a22fc8229f679fd
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-07-07 18:14:29 -0400

    now that equality operators exist, use them

--- src/supera/test/TestEvents.cxx
+++ src/supera/test/TestEvents.cxx
@@ -9,18 +9,18 @@ namespace supera
 
     bool VerifyEventMeta(const ImageMeta3D &computedMeta, const ImageMeta3D &expectedMeta)
     {
-      return false;
+      return computedMeta == expectedMeta;
     }
 
     bool VerifyEventLabels(const EventOutput &computedLabels, const EventOutput &expectedLabels)
     {
-      return false;
+      return computedLabels == expectedLabels;
     }
 
     std::map<std::string, TestEvent> TestEvents()
     {
       return {
-//      {"NumuCCIncEvt", NumuCCIncEvt()}
+          {"NumuCCIncEvt", NumuCCIncEvt()}
       };
     };
 
