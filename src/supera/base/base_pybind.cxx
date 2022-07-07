b4b463b2515e22c42a5c4de5828a329de6a8b57b
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-07-07 11:28:40 -0400

    bind the Point3D, EDep, and Vertex classes

--- src/supera/base/base_pybind.cxx
+++ src/supera/base/base_pybind.cxx
@@ -1,21 +1,16 @@
 
 #include "base_pybind.h"
-//#include "SuperaEvent.h"
 #include "SuperaType.h"
+#include "Point.h"
 
+#include "pybind11/operators.h"
 #include "supera/pybind_mkdoc.h"
 
 void init_base(pybind11::module& m)
 {
-/*
-  pybind11::class_<supera::ParticleLabel>(m, "ParticleGroup")
-  .def(pybind11::init<size_t&>())
-  .def(pybind11::init<size_t&>(), pybind11::arg("num_planes")=0)
-  ;
+  using namespace pybind11::literals;
 
-  pybind11::class_<supera::ImageLabel>(m, "ImageLabel")
-  ;
-*/
+  // types & consts from SuperaType.h
   pybind11::enum_<supera::SemanticType_t>(m, "SemanticType", DOC(supera, SemanticType_t))
       .value("Shower", supera::kShapeShower,  DOC(supera, SemanticType_t, kShapeShower))
       .value("Track", supera::kShapeTrack, DOC(supera, SemanticType_t, kShapeTrack))
@@ -56,5 +51,63 @@ void init_base(pybind11::module& m)
   m.attr("kINVALID_TRACKID") = supera::kINVALID_TRACKID;
   m.attr("kINVALID_VOXELID") = supera::kINVALID_VOXELID;
   m.attr("kINVALID_INSTANCEID") = supera::kINVALID_INSTANCEID;
+
+  // ----------------------------------------------------------------
+
+  // types & consts from Point.h
+  pybind11::class_<supera::Point3D>(m, "Point3D", DOC(supera, Point3D))
+      // constructors
+      .def(pybind11::init<>(), DOC(supera, Point3D, Point3D))
+      .def(pybind11::init<double, double, double>(), DOC(supera, Point3D, Point3D, 2),
+                          "xv"_a, "yv"_a, "zv"_a)
+      .def(pybind11::init<const supera::Point3D&>(), DOC(supera, Point3D, Point3D, 3),
+                          "pt"_a)
+
+      // overloaded operators
+      .def(pybind11::self == pybind11::self)
+      .def(pybind11::self != pybind11::self)
+      .def(pybind11::self /= double())
+      .def(pybind11::self *= double())
+      .def(pybind11::self += pybind11::self)
+      .def(pybind11::self -= pybind11::self)
+      .def(pybind11::self - pybind11::self)
+      .def(pybind11::self + pybind11::self)
+      .def(pybind11::self * double())
+      .def(pybind11::self / double())
+
+      // other methods
+      .def("squared_distance", &supera::Point3D::squared_distance, DOC(supera, Point3D, squared_distance), "pt"_a)
+      .def("distance", &supera::Point3D::distance, DOC(supera, Point3D, distance), "pt"_a)
+      .def("direction", &supera::Point3D::direction, DOC(supera, Point3D, direction), "pt"_a)
+
+      // member data
+      .def_readwrite("x", &supera::Point3D::x, DOC(supera, Point3D, x))
+      .def_readwrite("y", &supera::Point3D::y, DOC(supera, Point3D, y))
+      .def_readwrite("z", &supera::Point3D::z, DOC(supera, Point3D, z));
+
+  pybind11::class_<supera::EDep, supera::Point3D>(m, "EDep", DOC(supera, EDep))
+      .def(pybind11::init<>(), DOC(supera, EDep, EDep))
+      .def_readwrite("t", &supera::EDep::t, DOC(supera, EDep, t))
+      .def_readwrite("e", &supera::EDep::e, DOC(supera, EDep, e))
+      .def_readwrite("dedx", &supera::EDep::t, DOC(supera, EDep, dedx));
+
+  pybind11::class_<supera::Vertex>(m, "Vertex", DOC(supera, Vertex))
+      // constructors
+      .def(pybind11::init<>(), DOC(supera, Vertex, Vertex))
+      .def(pybind11::init<double, double, double, double>(), DOC(supera, Vertex, Vertex, 2))
+
+      // overloaded operators
+      .def(pybind11::self == pybind11::self)
+      .def(pybind11::self != pybind11::self)
+      .def(pybind11::self < pybind11::self)
+
+      // other methods
+      .def("approx", &supera::Vertex::approx, DOC(supera, Vertex, approx), "power"_a=6)
+      .def("dump", &supera::Vertex::dump, DOC(supera, Vertex, dump))
+
+      // data members
+      .def_readwrite("pos", &supera::Vertex::pos, DOC(supera, Vertex, pos))
+      .def_readwrite("time", &supera::Vertex::time, DOC(supera, Vertex, time));
+
 
 }
