5f5e977295582a827fd154e40028fbddd9d3cbf9
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-07-07 13:42:18 -0400

    bind BBox3D

--- src/supera/base/base_pybind.cxx
+++ src/supera/base/base_pybind.cxx
@@ -2,6 +2,7 @@
 #include "base_pybind.h"
 #include "SuperaType.h"
 #include "Point.h"
+#include "BBox.h"
 
 #include "pybind11/operators.h"
 #include "supera/pybind_mkdoc.h"
@@ -108,6 +109,52 @@ void init_base(pybind11::module& m)
       // data members
       .def_readwrite("pos", &supera::Vertex::pos, DOC(supera, Vertex, pos))
       .def_readwrite("time", &supera::Vertex::time, DOC(supera, Vertex, time));
+
+
+  // ----------------------------------------------------------------
+
+  // class from BBox.h
+  pybind11::class_<supera::BBox3D>(m, "BBox3D", DOC(supera, BBox3D))
+       // constructor
+      .def(pybind11::init<double,     double,     double,     double,     double,     double>(), DOC(supera, BBox3D, BBox3D),
+                          "xmin"_a=0, "ymin"_a=0, "zmin"_a=0, "xmax"_a=0, "ymax"_a=0, "zmax"_a=0)
+
+      // overloaded operator
+      .def(pybind11::self == pybind11::self)
+
+      // other methods
+      .def("update", pybind11::overload_cast<double, double, double, double, double, double>(&supera::BBox3D::update),
+           DOC(supera, BBox3D, update),
+           "xmin"_a, "ymin"_a, "zmin"_a, "xmax"_a, "ymax"_a, "zmax"_a)
+      .def("update", pybind11::overload_cast<const supera::Point3D&, const supera::Point3D&>(&supera::BBox3D::update),
+           DOC(supera, BBox3D, update, 2), "pt1"_a, "pt2"_a)
+      .def("empty", &supera::BBox3D::empty, DOC(supera, BBox3D, empty))
+      .def("origin", &supera::BBox3D::origin, DOC(supera, BBox3D, origin))
+      .def("bottom_left", &supera::BBox3D::bottom_left, DOC(supera, BBox3D, bottom_left))
+      .def("top_right", &supera::BBox3D::top_right, DOC(supera, BBox3D, top_right))
+      .def("center", &supera::BBox3D::center, DOC(supera, BBox3D, center))
+      .def("center_x", &supera::BBox3D::center_x, DOC(supera, BBox3D, center_x))
+      .def("center_y", &supera::BBox3D::center_y, DOC(supera, BBox3D, center_y))
+      .def("center_z", &supera::BBox3D::center_z, DOC(supera, BBox3D, center_z))
+      .def("min_x", &supera::BBox3D::min_x, DOC(supera, BBox3D, min_x))
+      .def("min_y", &supera::BBox3D::min_y, DOC(supera, BBox3D, min_y))
+      .def("min_z", &supera::BBox3D::min_z, DOC(supera, BBox3D, min_z))
+      .def("max_x", &supera::BBox3D::max_x, DOC(supera, BBox3D, max_x))
+      .def("max_y", &supera::BBox3D::max_y, DOC(supera, BBox3D, max_y))
+      .def("max_z", &supera::BBox3D::max_z, DOC(supera, BBox3D, max_z))
+      .def("width", &supera::BBox3D::width, DOC(supera, BBox3D, width))
+      .def("height", &supera::BBox3D::height, DOC(supera, BBox3D, height))
+      .def("depth", &supera::BBox3D::depth, DOC(supera, BBox3D, depth))
+      .def("volume", &supera::BBox3D::volume, DOC(supera, BBox3D, volume))
+      .def("area", &supera::BBox3D::area, DOC(supera, BBox3D, area), "axis"_a)
+      .def("overlap", &supera::BBox3D::overlap, DOC(supera, BBox3D, overlap), "box"_a)
+      .def("inclusive", &supera::BBox3D::inclusive, DOC(supera, BBox3D, inclusive), "box"_a)
+      .def("contains", pybind11::overload_cast<const supera::Point3D&>(&supera::BBox3D::contains, pybind11::const_),
+           DOC(supera, BBox3D, contains), "point"_a)
+      .def("contains", pybind11::overload_cast<double, double, double>(&supera::BBox3D::contains, pybind11::const_),
+           DOC(supera, BBox3D, contains),
+           "x"_a, "y"_a, "z"_a)
+      .def("dump", &supera::BBox3D::dump, DOC(supera, BBox3D, dump));
 
 
 }
