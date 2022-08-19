1000acc929a242f14c777090493581e232199f9a
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-08-19 18:01:46 -0400

    pybind for Voxel objects

--- src/supera/base/base_pybind.cxx
+++ src/supera/base/base_pybind.cxx
@@ -3,6 +3,7 @@
 #include "SuperaType.h"
 #include "Point.h"
 #include "BBox.h"
+#include "Voxel.h"
 
 #include "pybind11/operators.h"
 #include "supera/pybind_mkdoc.h"
@@ -88,9 +89,11 @@ void init_base(pybind11::module& m)
 
   pybind11::class_<supera::EDep, supera::Point3D>(m, "EDep", DOC(supera, EDep))
       .def(pybind11::init<>(), DOC(supera, EDep, EDep))
+      .def(pybind11::self == pybind11::self)
       .def_readwrite("t", &supera::EDep::t, DOC(supera, EDep, t))
       .def_readwrite("e", &supera::EDep::e, DOC(supera, EDep, e))
-      .def_readwrite("dedx", &supera::EDep::t, DOC(supera, EDep, dedx));
+      .def_readwrite("dedx", &supera::EDep::t, DOC(supera, EDep, dedx))
+      .def("dump", &supera::EDep::dump, DOC(supera, EDep, dump));
 
   pybind11::class_<supera::Vertex>(m, "Vertex", DOC(supera, Vertex))
       // constructors
@@ -156,5 +159,87 @@ void init_base(pybind11::module& m)
            "x"_a, "y"_a, "z"_a)
       .def("dump", &supera::BBox3D::dump, DOC(supera, BBox3D, dump));
 
+  // ----------------------------------------------------------------
 
-}
+   // classes from Voxel.h
+   pybind11::class_<supera::Voxel>(m, "Voxel", DOC(supera, Voxel))
+       // constructor
+      .def(pybind11::init<supera::VoxelID_t,               float>(), DOC(supera, Voxel, Voxel),
+                          "id"_a=supera::kINVALID_VOXELID, "value"_a=supera::kINVALID_FLOAT)
+
+                         
+      // overloaded operators
+      .def(pybind11::self == pybind11::self)
+      .def(pybind11::self == float())
+      .def(pybind11::self != pybind11::self)
+      .def(pybind11::self /= double())
+      .def(pybind11::self *= double())
+      .def(pybind11::self += float())
+      .def(pybind11::self -= float())
+      .def(pybind11::self < pybind11::self)
+      .def(pybind11::self < float())
+      .def(pybind11::self <= pybind11::self)
+      .def(pybind11::self <= float())
+      .def(pybind11::self > pybind11::self)
+      .def(pybind11::self > float())
+      .def(pybind11::self >= pybind11::self)
+      .def(pybind11::self >= float());
+
+    pybind11::class_<supera::VoxelSet>(m, "VoxelSet", DOC(supera, VoxelSet))
+        // constructor
+        .def(pybind11::init<>(), DOC(supera, VoxelSet, VoxelSet))
+        .def(pybind11::init<const supera::VoxelSet&>(), DOC(supera, VoxelSet, VoxelSet, 2), "vs"_a)
+
+        // overloaded operators
+        .def(pybind11::self == pybind11::self)
+        .def(pybind11::self != pybind11::self)
+        .def(pybind11::self /= float())
+        .def(pybind11::self *= float())
+        .def(pybind11::self += float())
+        .def(pybind11::self -= float())
+        // note: assignment operators not bound
+
+
+        // accessors
+        .def("id", pybind11::overload_cast<>(&supera::VoxelSet::id, pybind11::const_), DOC(supera, VoxelSet, id))
+        .def("as_vector", &supera::VoxelSet::as_vector, DOC(supera, VoxelSet, as_vector))
+        .def("find", &supera::VoxelSet::find, DOC(supera, VoxelSet, find), "id"_a)
+        .def("index", &supera::VoxelSet::index, DOC(supera, VoxelSet, index), "id"_a)
+        .def("sum", &supera::VoxelSet::sum, DOC(supera, VoxelSet, sum))
+        .def("mean", &supera::VoxelSet::mean, DOC(supera, VoxelSet, mean))
+        .def("max", &supera::VoxelSet::max, DOC(supera, VoxelSet, max))
+        .def("min", &supera::VoxelSet::min, DOC(supera, VoxelSet, min))
+        .def("size", &supera::VoxelSet::size, DOC(supera, VoxelSet, size))
+
+        // writeable stuff
+        .def("clear_data", &supera::VoxelSet::clear_data, DOC(supera, VoxelSet, clear_data))
+        .def("clear_invalid", &supera::VoxelSet::clear_invalid, DOC(supera, VoxelSet, clear_invalid),
+             "clear_invalid_float"_a=true, "clear_nan"_a=true, "clear_inf"_a=true)
+        .def("reserve", &supera::VoxelSet::reserve, DOC(supera, VoxelSet, reserve),
+             "num"_a)
+        .def("threshold", &supera::VoxelSet::threshold, DOC(supera, VoxelSet, threshold),
+             "min"_a, "max"_a)
+        .def("threshold_min", &supera::VoxelSet::threshold_min, DOC(supera, VoxelSet, threshold_min),
+             "min"_a)
+        .def("threshold_max", &supera::VoxelSet::threshold_max, DOC(supera, VoxelSet, threshold_max),
+             "max"_a)
+        .def("add", &supera::VoxelSet::add, DOC(supera, VoxelSet, add),
+             "vox"_a)
+        .def("insert", &supera::VoxelSet::insert, DOC(supera, VoxelSet, insert),
+             "vox"_a)
+        // can't bind an rvalue arg...
+//        .def("emplace", pybind11::overload_cast<supera::Voxel&&, const bool>(&supera::VoxelSet::emplace), DOC(supera, VoxelSet, emplace),
+//             "vox"_a, "add"_a)
+        .def("emplace", pybind11::overload_cast<supera::VoxelID_t, float, const bool>(&supera::VoxelSet::emplace),
+             DOC(supera, VoxelSet, emplace, 2),
+             "id"_a, "value"_a, "add"_a)
+        .def("emplace", pybind11::overload_cast<const supera::VoxelSet&, const bool>(&supera::VoxelSet::emplace),
+             DOC(supera, VoxelSet, emplace, 3),
+             "vs"_a, "add"_a)
+        .def("paint", &supera::VoxelSet::paint, DOC(supera, VoxelSet, paint),
+             "value"_a)
+
+        .def("id", pybind11::overload_cast<const supera::InstanceID_t>(&supera::VoxelSet::id), DOC(supera, VoxelSet, id, 2), "id"_a);
+
+
+ }
