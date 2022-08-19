95fb816d571f54bc7d61a2331134cfb779e7d782
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-08-19 18:02:23 -0400

    dig deeper when a test fails

--- test/test_events.py
+++ test/test_events.py
@@ -7,13 +7,40 @@ bbox_cfg = {
 }
 
 label_cfg = {
-    "LogLevel": "VERBOSE",
+    "LogLevel": "INFO",
 
     "EnergyDepositThreshold":  "0",
     "UseSimEnergyDeposit":       "True",  # currently unused but required parameter
     "UseSimEnergyDepositPoints": "False", # ditto
 
 }
+
+def prop_compare(exp, recv, indent=0):
+    all_ok = True
+    for prop_name in dir(exp):
+#        print("   property name:", prop_name)
+        prop_exp = getattr(exp, prop_name)
+        prop_recv = getattr(recv, prop_name)
+        if hasattr(getattr(exp, prop_name), "__call__"):
+            continue
+
+        if prop_exp == prop_recv:
+            continue
+
+        all_ok = False
+        indent_txt = ("  " * indent)
+        print(indent_txt, "   --> Mismatch in property '%s':" % prop_name)
+        if prop_name in ("energy", "dedx"):
+            for txt, label in {"Expected": exp, "Received": recv}.items():
+                print(txt, getattr(label, prop_name).size(), "voxels summing to", getattr(label, prop_name).sum())
+        elif prop_name == "part":
+            prop_compare(getattr(exp, prop_name), getattr(recv, prop_name), indent+1)
+
+        else:
+            print(indent_txt, "     Expected:", prop_exp.dump() if hasattr(prop_exp, "dump") else prop_exp)
+            print(indent_txt, "     Received:", prop_recv.dump() if hasattr(prop_recv, "dump") else prop_recv)
+
+    print("all ok:", all_ok)
 
 def test_events():
     import supera
@@ -38,15 +65,13 @@ def test_events():
         if not supera.test.VerifyEventLabels(driver.Label(), testev.output_labels):
             same = False
             print("Labels for test event '%s' disagree with expectation." % evname)
-            print("Expected particle content:")
-            for label in testev.output_labels.particles:
-                print(label.dump())
-            print("Received particle content:")
-            for label in driver.Label().particles:
-                print(label.dump())
 
-        with open("/tmp/%s.ixx" % evname, "w") as outf:
-            outf.write(driver.Label().dump2cpp())
+            for i, (label_exp, label_recv) in enumerate(zip(testev.output_labels.particles, driver.Label().particles)):
+                print(" label", i)
+                if label_exp != label_recv:
+                    print("label #%d disagrees:" % i)
+
+                    prop_compare(label_exp, label_recv)
 
         if not same:
             raise RuntimeError("Event #%d ('%s') failed test!" % (evnum, evname))
